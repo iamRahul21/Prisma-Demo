99fb0723d254b7a32a07095ce9495131196ba3c1
iamRahul21 <rahuldeka072@gmail.com>
2024-02-01 16:43:21 +0530

    tsc done

--- build/index.js
+++ build/index.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express")); // or we can write const express = require('express')
+const express_1 = __importDefault(require("express"));
 const user_1 = __importDefault(require("./user"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
--- build/user.js
+++ build/user.js
@@ -12,8 +12,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 const client_1 = require("@prisma/client");
-const express_1 = __importDefault(require("express"));
 const prisma = new client_1.PrismaClient();
+const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 //GET
 router.get('/user', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -45,6 +45,61 @@ router.post('/addUser', (req, res) => __awaiter(void 0, void 0, void 0, function
     }
     catch (error) {
         console.log(error);
+    }
+}));
+//PUT
+router.put('/updateUser/:userID', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const userID = req.params.userID;
+    try {
+        const existingUser = yield prisma.user.findUnique({
+            where: {
+                id: userID
+            }
+        });
+        if (!existingUser) {
+            return res.status(404).send("user not found");
+        }
+        const { name, age, role, email } = req.body;
+        const updatedUser = yield prisma.user.update({
+            where: {
+                id: userID
+            },
+            data: {
+                name: name,
+                age: age,
+                role: role,
+                email: email
+            },
+        });
+        res.send(updatedUser);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server error");
+    }
+}));
+//DELETE
+router.delete('/deleteUser/:userID', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const userID = req.params.userID; //we are using uuid
+    try {
+        const existingUser = yield prisma.user.findUnique({
+            where: {
+                id: userID
+            }
+        });
+        if (!existingUser) {
+            return res.status(404).send("user not found");
+        }
+        yield prisma.user.delete({
+            where: {
+                id: userID
+            }
+        });
+        res.send("User deleted successfully");
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error");
     }
 }));
 module.exports = router;
